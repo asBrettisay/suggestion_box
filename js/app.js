d9fc9108af4dcc76fb4ffaba8ac58f809a3714ed
asBrettisay <brett.caudill.is@gmail.com>
2015-11-10 20:08:59 -0800

    Uncaught Error: Injection Modulerr
    Getting an uncaught error in this code. I’ve been trying to install
    routing. Previous build was working correctly.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-var app = angular.module('SuggestionBox', []);
+var app = angular.module('SuggestionBox', ['ngRoute']);
+app.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    controller: 'HomeController',
+    templateUrl: 'views/home.html'
+  });
+});
--- js/controllers/homecontrollers.js
+++ js/controllers/homecontrollers.js
@@ -1,24 +1,3 @@
-app.controller('HomeController', [
-	'$scope',
-	'suggestions',
-
-	function($scope, suggestions) {
-	$scope.posts = suggestions.posts
-
-	$scope.addSuggestion = function() {
-		if ($scope.title = undefined) {
-			return
-		}
-		else {
-			$scope.posts.push({
-				title: $scope.title,
-				upvotes: 0,
-			});
-			$scope.title = "";
-		}
-
-	};
-	$scope.upVote = function(post) {
-		post.upvotes += 1;
-	};
+app.controller('HomeController', ['$scope', function($scope) {
+	$scope.helloWorld = "Hello, AngularJS!";
 }]);
