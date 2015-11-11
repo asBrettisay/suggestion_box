95f6bc7960677b6c86e479a46504db5fb05d7107
asBrettisay <brett.caudill.is@gmail.com>
2015-11-10 20:23:34 -0800

    Still stuck here

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('SuggestionBox', ['ngRoute']);
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
   $routeProvider
   .when('/', {
     controller: 'HomeController',
